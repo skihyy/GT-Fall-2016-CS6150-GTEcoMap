ac903cfc17903ab3e80d1e0b623cb514413eaedf
Aoyi Li <ali327@gatech.edu>
2016-11-27 01:33:17 -0500

    Further implementation of map
    1. Add color to nodes
    
    2. Svg Canvas is dynamically changing height based on node’s number
    
    3. Node grey-out feature implemented
    
    4. Hyper link text for both person/project nodes implemented
    
    5. Node avatar image for person
    
    6. Few minor changes in original database for special cases

--- Map/map.js
+++ Map/map.js
@@ -7,7 +7,7 @@
  * @param name name of check box
  * @param idOfForm id of the form
  */
-function selectAll(obj, name, idOfForm) {
+ function selectAll(obj, name, idOfForm) {
     var checkboxs = document.getElementsByName(name);
     for (var i = 0; i < checkboxs.length; i++) {
         checkboxs[i].checked = obj.checked;
@@ -22,7 +22,7 @@ function selectAll(obj, name, idOfForm) {
  * @param nameOfChkBox checkbox name
  * @param idOfForm id of the form
  */
-function checkSelectAll(idOfSelectAll, nameOfChkBox, idOfForm) {
+ function checkSelectAll(idOfSelectAll, nameOfChkBox, idOfForm) {
     var checkboxs = document.getElementsByName(nameOfChkBox);
     var isAllSelected = true;
     for (var i = 0; i < checkboxs.length; i++) {
@@ -39,7 +39,7 @@ function checkSelectAll(idOfSelectAll, nameOfChkBox, idOfForm) {
  * Submit form.
  * @param id id of form
  */
-function submitForm(id) {
+ function submitForm(id) {
     document.getElementById(id).submit();
 }
 
@@ -47,7 +47,7 @@ function submitForm(id) {
  * Visualization implementation.
  * @param data json data from DB
  */
-function draw(data) {
+ function draw(data) {
     var nodes = data.nodes;
     var links = data.links;
 
@@ -56,24 +56,30 @@ function draw(data) {
     // Compute the distinct nodes from the links.
     links.forEach(function (link) {
         link.source = nodes[link.source] ||
-            (nodes[link.source] = {name: link.source});
+        (nodes[link.source] = {name: link.source});
         link.target = nodes[link.target] ||
-            (nodes[link.target] = {name: link.target});
+        (nodes[link.target] = {name: link.target});
+
+        //console.log(link);
     });
 
+
+    // Set the width
     var width = 960,
-        height = 500,
+        //for every 15 node, add another 500 height
+        height = (~~(nodes.length / 15)+1) * 500,
         color = d3.scale.category20c();
 
 
+    // Generate force
     var force = d3.layout.force()
-        .nodes(d3.values(nodes))
-        .links(links)
-        .size([width, height])
-        .linkDistance(120)
-        .charge(-800)
-        .on("tick", tick)
-        .start();
+    .nodes(d3.values(nodes))
+    .links(links)
+    .size([width, height])
+    .linkDistance(130)
+    .charge(-1200)
+    .on("tick", tick)
+    .start();
 
     // Set the range
     var v = d3.scale.linear().range([0, 100]);
@@ -83,105 +89,198 @@ function draw(data) {
         return d.value;
     })]);
     
+    // Initilize svg canvas
     var svg = d3.select("#d3AreaMain").append("svg")
-        .attr("width", width)
-        .attr("height", height);
+    .attr("width", width)
+    .attr("height", height);
 
 
     // add the links and the arrows
     var path = svg.append("svg:g").selectAll("path")
-        .data(force.links())
-        .enter().append("svg:path")
-        .attr("class", function (d) {
-            return "link " + d.type;
-        });
+    .data(force.links())
+    .enter().append("svg:path")
+    .attr("class", function (d) {
+        return "link " + d.type;
+    });
 
+    // This clipPath is for later cropping the avatar image for "person" node
+    defs = svg.append('defs');
+    var clipPath = defs.append('clipPath')
+    .attr('id', 'clip-circle')
+    .append('circle')
+    .attr('r', 20);
 
-    // change the link color
-    var link2 = d3.select("#d3AreaMain").selectAll("path.link")
-        .style("stroke", function (d) {
-            return "black";
-        })
+    // define the links
+    var link = d3.select("#d3AreaMain").selectAll("path.link")
+    .style("stroke", function (d) {
+        return "black";
+    })
 
-    link2.style("visibility", "hidden");
+    // hide all the links in default
+    link.style("visibility", "hidden");
 
     // define the nodes
     var node = svg.selectAll(".node")
-        .data(force.nodes())
-        .enter().append("g")
-        .attr("class", "node")
-        .call(force.drag);
+    .data(force.nodes())
+    .enter().append("g")
+    .attr("class", "node")
+    .call(force.drag);
 
-    // add the nodes
+    // add the circle
     node.append("circle")
-        .attr("r", function (d) {
-            if (d.type == "person") {
-                return 20;
-            }
-            else {
-                return 50 * Math.sqrt(d.weight);
-            }
-        });
+    // fill with the node color
+    .attr("fill", function (d) { 
+        if(d.color=="") return "#CCC";
+        else return d.color;
+    })
+    // set the size of the circle
+    .attr("r", function (d) {
+        // person node is one-size for all
+        if (d.type == "person") {
+            return 20;
+        }
+        // project node size is based on how many people work on it 
+        else {
+            return 60 * Math.sqrt(d.weight);
+        }
+    });
 
-    node.transition().attr("visibility", function (d) {
+    // hide all the person node in default
+    node.style("visibility", function (d) {
         if (d.type == "person") {
             return "hidden";
         }
     })
-    // add the node name
-    node.append("foreignObject")
-        .attr("x", -45)
-        .attr("y", -30)
-        .attr("width", function (d) {
-            return 2 * 20;
-        })
-        .attr("height", function (d) {
-            return 2 * 20;
-        })
-        .append("xhtml:body")
-        .html(function (d) {
-            return d.name;
-        });
 
-    // pining the node
+    // add avatar image for person node
+    node.filter(function(d) { 
+        return d.type == "person"; 
+    })
+    .append("svg:image")
+    .attr("xlink:href","http://safariuganda.com/wp-content/uploads/2014/12/480px-Facebook-default-no-profile-pic.jpg")
+    .attr("height",20*2)
+    .attr("width",20*2)
+    .attr("x","-"+20)
+    .attr("y","-"+20)
+    .attr('clip-path', 'url(#clip-circle)');
+
+
+    // add the project's name
+    // Text-warpping is bit complicate in d3 circles, foreignObject is what we decide to use
+    // Use foreignObject does creatre some inconsistency and it has some flaws, replace it if any better options has been found
+    node.filter(function(d) { 
+        return d.type == "project"; })
+    .append("foreignObject")
+    .attr("x", -45)
+    .attr("y", -30)
+    .attr("width", function (d) {
+        return 80;
+    })
+    .attr("height", function (d) {
+        return 80;
+    })
+    .append("xhtml:body")
+    .html(function (d) {
+        return d.name;
+    })
+    // add the project's hyperlink if it has any
+    .append("foreignObject")
+    .filter(function(d) { 
+        return d.link != null; })
+    .append("xhtml:body")
+    .html(function (d) {
+        return "link";
+    })
+    .on("click", function(d) { 
+        window.open("http://"+d.link); 
+    })
+    .style("color","blue")
+    .style("text-decoration","underline")
+    .style("cursor","pointer")
+
+
+
+    // add the person's name
+    node.filter(function(d) { return d.type == "person"; })
+    .append("text")
+    .attr("dy",30)
+    .attr("dx",15)
+    .text(function(d){
+        return d.name;
+    })
+
+    // add the person's hyperlink text if a person has a link
+    node.filter(function(d) { return d.type == "person"; })
+    .filter(function(d) { 
+        return d.link != null; 
+    })
+    .append("text")
+    .text("link")
+    .on("click", function(d) { 
+        window.open("http://"+d.link); 
+    })
+    .attr("dy",45)
+    .attr("dx",35)
+    .style("stroke","blue")
+    .style("text-decoration","underline")
+    .style("cursor","pointer")
+
+
+
+    //clicking the node logic
     node.on("click", function (d) {
         //console.log(d.id);
+        // show all the links that connect to this node
+        // store all the node that connect to this node to a list for later check
         var list = [];
-        link2.transition()
-            .style("visibility", function (e) {
+        link.transition()
+        .style("visibility", function (e) {
 
-                if (e.source.id == d.id || e.target.id == d.id) {
-                    list.push(e.source.id);
-                    list.push(e.target.id);
-                    return "visible";
-                }
-                else return "hidden";
-            })
+            if (e.source.id == d.id || e.target.id == d.id) {
+                list.push(e.source.id);
+                list.push(e.target.id);
+                return "visible";
+            }
+            else return "hidden";
+        })
+        //console.log(list);
+        // gery-out all the other projects that not in the previous stored list
         node.transition()
-            .style("visibility", function (e) {
+        .style("opacity", function (e) {
+            if(e.type=="project"){
+             return (list.indexOf(e.id) > -1) ? "1" : "0.2"; 
+         }
+
+     })
+        // show the person that stored in the previous list
+        .style("visibility", function (e) {
+            if(e.type=="person"){
+                console.log(e.id);
                 console.log(list.indexOf(e.id));
-                return (list.indexOf(e.id) > -1) ? "visible" : "hidden";
-            })
+                return (list.indexOf(e.id) > -1) ? "visible" : "hidden"; 
+            }
+
+        })
+        
     });
 
     // add the curvy lines
     function tick() {
         path.attr("d", function (d) {
             var dx = d.target.x - d.source.x,
-                dy = d.target.y - d.source.y,
-                dr = Math.sqrt(dx * dx + dy * dy);
+            dy = d.target.y - d.source.y,
+            dr = Math.sqrt(dx * dx + dy * dy);
             return "M" +
-                d.source.x + "," +
-                d.source.y + "A" +
-                dr + "," + dr + " 0 0,1 " +
-                d.target.x + "," +
-                d.target.y;
+            d.source.x + "," +
+            d.source.y + "A" +
+            dr + "," + dr + " 0 0,1 " +
+            d.target.x + "," +
+            d.target.y;
         });
 
-        node
-            .attr("transform", function (d) {
-                return "translate(" + d.x + "," + d.y + ")";
-            });
+        node.attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
     };
 
 }
